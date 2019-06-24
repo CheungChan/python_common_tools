97d0b0e44b15c43d6fab5d5b298bed6fe59d7417
香饽饽 <1377699408@qq.com>
2019-06-24 21:45:09 +0800

    sh

--- python_common_tools/bin/az_nohup_start.sh
+++ python_common_tools/bin/az_nohup_start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-app_name=$1
+proc=$@
 
-help="阿章专用脚本:后台启动python脚本:$app_name"
+help="阿章专用脚本:后台启动任务:$proc"
 
 # 绿色输出提示
 echo -e "\033[32m $help \033[0m"
 
-nohup python "$app_name" > "nohup.$app_name.out" 2>&1 &
+nohup python "$proc" > "nohup.$proc.out" 2>&1 &
