73d200b090a7809850c103c4b9d50215860400d3
香饽饽 <1377699408@qq.com>
2019-06-24 21:31:57 +0800

    sh

--- python_common_tools/bin/az_nohup_start.sh
+++ python_common_tools/bin/az_nohup_start.sh
@@ -7,4 +7,4 @@ help="阿章专用脚本:后台启动python脚本:$app_name"
 # 绿色输出提示
 echo -e "\033[32m $help \033[0m"
 
-nohup python $app_name >nohup.$app_name.out 2>&1 &
+nohup python "$app_name" > "nohup.$app_name.out" 2>&1 &
--- python_common_tools/bin/az_ps_check.sh
+++ python_common_tools/bin/az_ps_check.sh
@@ -7,4 +7,4 @@ help="阿章专用脚本:进程检查:$app_name"
 
 # 绿色输出提示
 echo -e "\033[32m $help \033[0m"
-ps -ef|grep $app_name|grep -v grep
+ps -ef|grep "$app_name"|grep -v grep
