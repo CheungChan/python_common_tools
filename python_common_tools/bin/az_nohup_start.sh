9a6fe5236f5dbfa0da2d8a68b80e823065f8f555
香饽饽 <1377699408@qq.com>
2019-06-24 21:52:37 +0800

    sh

--- python_common_tools/bin/az_nohup_start.sh
+++ python_common_tools/bin/az_nohup_start.sh
@@ -7,4 +7,7 @@ help="阿章专用脚本:后台启动任务:$proc"
 # 绿色输出提示
 echo -e "\033[32m $help \033[0m"
 
-nohup python "$proc" > "nohup.$proc.out" 2>&1 &
+# 将所有命令中的空格替换成_
+# shell字符串替换用法  单个替换${variable/before/after}  全部替换 ${variable//before/after}
+proc_no_blank=${proc//' '/'_'}
+nohup python "$proc" > "nohup.$proc_no_blank.out" 2>&1 &
