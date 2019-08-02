4dd0ebcfe5ea9a68fa2a739f45ae7303155c600d
香饽饽 <1377699408@qq.com>
2019-08-02 17:05:02 +0800

    scp

--- python_common_tools/bin/az_scp_to.sh
+++ python_common_tools/bin/az_scp_to.sh
@@ -3,8 +3,8 @@
 port=$1
 src_path=$2
 to_path=$3
-help="阿章专用脚本:从ssh本地端口 $port 转发的 $src_path 拷贝文件到  $to_path"
+help="阿章专用脚本:从 $src_path 拷贝文件到 ssh本地端口 $port 转发的 $to_path"
 
 # 绿色输出提示
 echo -e "\033[32m $help \033[0m"
-scp -r -P $port root@localhost:$src_path $to_path
+scp -r -P $port $src_path root@localhost:$to_path
