65b3497c32633dc0e21133486caa7f811e090dce
香饽饽 <1377699408@qq.com>
2019-11-06 15:59:17 +0800

    init

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ pre-commit
 install_requires = [i for i in requirements.split() if i]
 setup(
     name="python_common_tools",
-    version="2.1.1",
+    version="2.1.3",
     python_requires=">3.4.0",
     author="chenzhang",
     author_email="1377699408@qq.com",
